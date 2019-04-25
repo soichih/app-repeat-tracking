8ec1ab0b50c90501d4c4d8c88183856d861e05e0
bcmcpher <bcmcpher@iu.edu>
2019-04-25 16:46:27 -0400

    fixed app to run w/in our mcr container

--- clean.sh
+++ clean.sh
@@ -2,3 +2,13 @@
 
 rm -rf intermediate
 rm -rf rep*
+
+rm rep02/labels_dilated.nii.gz
+rm rep03/labels_dilated.nii.gz
+rm rep04/labels_dilated.nii.gz
+rm rep05/labels_dilated.nii.gz
+rm rep06/labels_dilated.nii.gz
+rm rep07/labels_dilated.nii.gz
+rm rep08/labels_dilated.nii.gz
+rm rep09/labels_dilated.nii.gz
+rm rep10/labels_dilated.nii.gz
--- network_jobs.sh
+++ network_jobs.sh
@@ -7,11 +7,13 @@ set -x
 
 #module load matlab/R2018b
 
-export MCRROOT=/cvmfs/connect.opensciencegrid.org/modules/packages/linux-rhel7-x86_64/gcc-4.8.5spack/matlab-R2018b-kxlw75gnubxtxbwm35774ulervu7h5fd/v95
+#export MCRROOT=/cvmfs/connect.opensciencegrid.org/modules/packages/linux-rhel7-x86_64/gcc-4.8.5spack/matlab-R2018b-kxlw75gnubxtxbwm35774ulervu7h5fd/v95
+export MCRROOT=/opt/mcr/v92
+
 export LD_LIBRARY_PATH=${MCRROOT}/runtime/glnxa64
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/bin/glnxa64
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/os/glnxa64
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/opengl/lib/glnxa64
 
-chmod +x ./main
-./main $1
+chmod +x ./compiled/main
+./compiled/main $1
