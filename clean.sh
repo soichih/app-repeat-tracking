ad438d170d0af8d7685bc5740c166ca2f9da6bc2
bcmcpher <bcmcpher@iu.edu>
2019-04-25 16:52:46 -0400

    updated clean.sh to point to right folder

--- clean.sh
+++ clean.sh
@@ -3,12 +3,12 @@
 rm -rf intermediate
 rm -rf rep*
 
-rm rep02/labels_dilated.nii.gz
-rm rep03/labels_dilated.nii.gz
-rm rep04/labels_dilated.nii.gz
-rm rep05/labels_dilated.nii.gz
-rm rep06/labels_dilated.nii.gz
-rm rep07/labels_dilated.nii.gz
-rm rep08/labels_dilated.nii.gz
-rm rep09/labels_dilated.nii.gz
-rm rep10/labels_dilated.nii.gz
+rm net02/labels_dilated.nii.gz
+rm net03/labels_dilated.nii.gz
+rm net04/labels_dilated.nii.gz
+rm net05/labels_dilated.nii.gz
+rm net06/labels_dilated.nii.gz
+rm net07/labels_dilated.nii.gz
+rm net08/labels_dilated.nii.gz
+rm net09/labels_dilated.nii.gz
+rm net10/labels_dilated.nii.gz
